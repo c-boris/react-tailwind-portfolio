4167479a762e10e209332eb63a8382f61b8ae9fc
cboris <b.crochet@gmail.com>
2023-07-01 23:08:21 +0200

    fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ import '../index.css';
 const Home = () => {
   return (
     
-    <div name='home' className='w-full h-screen sm:h-screen'>
+    <div name='home' className='w-full h-screen sm:h-screen bg-[#0a192f]'>
       
       {/* Container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
