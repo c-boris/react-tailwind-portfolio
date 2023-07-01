3faaae3caa641948e69bcf5f64624f963d801211
cboris <b.crochet@gmail.com>
2023-07-02 01:42:08 +0200

    fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-400'>
+    <div name='about' className='w-full h-fit bg-[#0a192f] pt-24 sm:h-screen text-gray-400'>
       <div className='flex flex-col justify-center items-center w-full h-full'>
         <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
           <div className='sm:text-right pb-8 pl-4'>
