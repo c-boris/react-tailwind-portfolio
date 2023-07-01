845ff945da7d5986c7645995f7ba2d03124befae
cboris <b.crochet@gmail.com>
2023-07-01 19:50:20 +0200

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
+import ParticlesBg from "particles-bg";
 import { Link } from 'react-scroll';
+import '../index.css';
 
 
 const Home = () => {
   return (
-    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+    
+    <div name='home' className='w-full h-screen '>
+      <ParticlesBg color="#0c387b" num={5} type="circle" bg={true} />
+      {/* <ParticlesBg color="#eeeeee" num={50} type="cobweb" bg={true} /> */}
 
       {/* Container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-        <p className='text-blue-600'>Hi, my name is</p>
+        <p className='text-blue-600 text-xl'>Hi, my name is</p>
         <h1 className='text-4xl smtext-7xl font-bold text-gray-200'>Bob Doe</h1>
         <h2 className='text-4xl smtext-7xl font-bold text-gray-400'>I'm a Full Stack Developer.</h2>
         <p className='text-gray-400 py-4 max-w-[700px]'>I’m a full-stack developer specializing in building (and occasionally
           designing) exceptional digital experiences. Currently, I’m focused on
           building responsive full-stack web applications.</p>
         <div>
-          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center rounded-md hover:bg-blue-600'>
+          <button className='text-gray-200 border-2 border-gray-400 px-6 py-3 my-2 flex items-center rounded-md hover:bg-blue-600'>
           <Link to='work' smooth={true} duration={500}>
             View Work
           </Link>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa';
 import {HiOutlineMail} from 'react-icons/hi';
-import {BsFillPersonLinesFill} from 'react-icons/bs';
+// import {BsFillPersonLinesFill} from 'react-icons/bs';
 import { Link } from 'react-scroll';
 import { DiCode } from "react-icons/di";
 
@@ -10,47 +10,47 @@ const Navbar = () => {
     const handleClick = () => setNav(!nav)
   
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-200'>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-200 '>
       <div>
         <Link to='home' smooth={true} duration={500}>
-        <DiCode className='w-10 h-10 cursor-pointer'/>
+        <DiCode className='w-10 h-10 cursor-pointer hover:text-gray-400'/>
         </Link>
       </div>
 
       {/* Menu */}
       
         <ul className='hidden md:flex'>
-        <li>
-          <Link to='home' smooth={true} duration={500}>
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to='about' smooth={true} duration={500}>
-            About
-          </Link>
-        </li>
-        <li>
-          <Link to='skills' smooth={true} duration={500}>
-            Skills
-          </Link>
-        </li>
-        <li>
-          <Link to='work' smooth={true} duration={500}>
-            Work
-          </Link>
-        </li>
-        <li>
-          <Link to='contact' smooth={true} duration={500}>
-            Contact
-          </Link>
-        </li>
+          <li className='hover:text-gray-400'>
+            <Link to='home' smooth={true} duration={500}>
+              Home
+            </Link>
+          </li>
+          <li className='hover:text-gray-400'>
+            <Link to='about' smooth={true} duration={500}>
+              About
+            </Link>
+          </li>
+          <li className='hover:text-gray-400'>
+            <Link to='skills' smooth={true} duration={500}>
+              Skills
+            </Link>
+          </li>
+          <li className='hover:text-gray-400'>
+            <Link to='work' smooth={true} duration={500}>
+              Work
+            </Link>
+          </li>
+          <li className='hover:text-gray-400'>
+            <Link to='contact' smooth={true} duration={500}>
+              Contact
+            </Link>
+          </li>
         </ul>
       
 
       
       {/* Hamburger */}
-      <div onClick={handleClick} className='md:hidden z-10 cursor-pointer '>
+      <div onClick={handleClick} className='md:hidden z-10 cursor-pointer hover:text-gray-400'>
         {!nav ? <FaBars className="w-6 h-6"/> : <FaTimes className="w-6 h-6"/>}
       </div>
 
@@ -62,30 +62,30 @@ const Navbar = () => {
             : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
         }
       >
-        <li className='py-6 text-4xl'>
+        <li className='py-6 text-3xl hover:text-gray-400'>
           <Link onClick={handleClick} to='home' smooth={true} duration={500}>
             Home
           </Link>
         </li>
-        <li className='py-6 text-4xl'>
+        <li className='py-6 text-3xl hover:text-gray-400'>
           {' '}
           <Link onClick={handleClick} to='about' smooth={true} duration={500}>
             About
           </Link>
         </li>
-        <li className='py-6 text-4xl'>
+        <li className='py-6 text-3xl hover:text-gray-400'>
           {' '}
           <Link onClick={handleClick} to='skills' smooth={true} duration={500}>
             Skills
           </Link>
         </li>
-        <li className='py-6 text-4xl'>
+        <li className='py-6 text-3xl hover:text-gray-400'>
           {' '}
           <Link onClick={handleClick} to='work' smooth={true} duration={500}>
             Work
           </Link>
         </li>
-        <li className='py-6 text-4xl'>
+        <li className='py-6 text-3xl hover:text-gray-400'>
           {' '}
           <Link onClick={handleClick} to='contact' smooth={true} duration={500}>
             Contact
@@ -94,32 +94,32 @@ const Navbar = () => {
       </ul>
 
       {/* Social icons */}
-      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+      <div className='hidden lg:flex fixed flex-col top-[40%] left-0'>
         <ul>
-        <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-800'>
+        <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] rounded-md hover:ml-[-10px] duration-300 bg-blue-800'>
             <a className='flex justify-between items-center w-full text-gray-300'
-            href="/">
+            href="https://www.linkedin.com/">
               Linkedin <FaLinkedin size={30}/>
             </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-gray-800'>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] rounded-md hover:ml-[-10px] duration-300 bg-gray-800'>
             <a className='flex justify-between items-center w-full text-gray-300'
-            href="/">
+            href="https://github.com/">
               Github <FaGithub size={30}/>
             </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-800'>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] rounded-md hover:ml-[-10px] duration-300 bg-blue-800'>
             <a className='flex justify-between items-center w-full text-gray-300'
-            href="/">
+            href="mailto:dev@gmail.com">
               Email <HiOutlineMail size={30}/>
             </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-gray-800'>
+          {/* <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] rounded-md hover:ml-[-10px] duration-300 bg-gray-800'>
             <a className='flex justify-between items-center w-full text-gray-300'
             href="/">
               Resume <BsFillPersonLinesFill size={30}/>
             </a>
-          </li>
+          </li> */}
         </ul>
       </div>
     </div>
