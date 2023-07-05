ec0135ac464ea06988f8b3c7a4484d3cca144882
cboris <b.crochet@gmail.com>
2023-07-05 14:00:46 +0200

    fix

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -22,9 +22,7 @@ const Work = () => {
             <div
               key={index}
               style={{ backgroundImage: `url(${item.image})` }}
-              className='shadow-lg shadow-[#040c16] group container rounded-md 
-              flex justify-center text-center items-center mx-auto content-div'
-            >
+              className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div'>
               {/* Hover effect for images */}
               <div className='opacity-0 group-hover:opacity-100'>
                 <span className='text-2xl font bold text-white tracking-wider'>
@@ -32,16 +30,13 @@ const Work = () => {
                 </span>
                 <div className='pt-8 text-center'>
                   {/* eslint-disable-next-line */}
-                  <a href={item.github} target='_blank'>
-                    <button
-                      className='text-center rounded-lg px-4 py-3 m-2
-                      bg-white text-gray-700 font-bold text-lg'
-                    >
+                  <a href={item.github} target='_blank' rel='noopener noreferrer'>
+                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                       Code
                     </button>
                   </a>
                   {/* eslint-disable-next-line */}
-                  <a href={item.live} target='_blank'>
+                  <a href={item.live} target='_blank' rel='noopener noreferrer'>
                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                       Live
                     </button>
